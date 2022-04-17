54d39842ae0fd25b309bc5caea849e9042d3e518
ADMIN <ADMIN@DESKTOP-OEIQSEI>
2022-04-17 13:29:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleApp50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nik";
+            string familyName = "Kond";
+            Console.WriteLine(name + " " + familyName);
+            string changedData = name;
+            name = familyName;
+            familyName = changedData;
+            Console.WriteLine(name + " " + familyName);
+        }
+    }
+}
